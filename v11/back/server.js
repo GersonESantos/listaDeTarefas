eca5eae79e54002cfd8a8dae855303064855728a
GersonESS <gebhsantos@gmail.com>
2025-02-18 18:39:23 -0300

    a

---
+++ v11/back/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+// opcoes de conexao com o MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'bd_tasks'
+});
+
+const app = new express();
+app.listen(3000, () => {
+    console.log('Servidor iniciado.');
+})
+app.use(cors());
+// rotas
+// ----------------------------------------
+app.get("/", (req, res) => {
+
+    // res.send('Olá Mundo!');
+    connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
+        if(err){
+            res.send('MySQL connection error.');
+        }
+        res.send('MySQL connection OK.');
+    })
+});
+
+// ----------------------------------------
+app.get("/user/:id", (req, res) => {
+    connection.query("SELECT id, username, created_at FROM users WHERE id = ?", [req.params.id] ,(err, results) => {
+        if(err){
+            res.send('MySQL connection error.');
+        }
+        res.json(results);
+    })
+});
+

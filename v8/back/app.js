5404ca5d9b511fd04365fb4ba34368a3667fe207
GersonESS <gebhsantos@gmail.com>
2025-02-17 21:03:46 -0300

    a

---
+++ v8/back/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require("mysql2");
+const app = express();
+
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Gabibi89*",
+  database: "projeto"
+});
+
+db.connect(err => {
+  if (err) throw err;
+  console.log("Banco de dados conectado!");
+});
+
+app.get('/', (req, res) => {
+  res.write('Ola! digite http://localhost:3000/clientes para ver os clientes');
+  res.end()
+});
+
+app.get("/clientes", (req, res) => {
+  db.query("SELECT * FROM cliente", (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});

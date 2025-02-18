3ae648121a4026415c2a601dd179eec82ad1392a
GersonESS <gebhsantos@gmail.com>
2025-02-18 12:19:31 -0300

    a

--- bk/server.js
+++ bk/server.js
@@ -1,38 +1,35 @@
 const express = require('express');
-const mysql = require("mysql2");
+const mysql = require('mysql');
 const cors = require('cors');
 
 // opcoes de conexao com o MySQL
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Gabibi89*",
-  database: "bd_tasks"
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bd_tasks'
 });
+
 const app = new express();
-
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
-
-connection.connect(err => {
-  if (err) throw err;
-  console.log("Banco de dados conectado!");
-});
-
+    console.log('Servidor iniciado.');
+})
+ 
 app.use(cors());
+
 // rotas
 // ----------------------------------------
 app.get("/", (req, res) => {
 
-  connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
-      if(err){
-          res.send('MySQL connection error.');
-      }
-      res.send('MySQL connection OK.');
-  })
+    // res.send('Olá Mundo!');
+    connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
+        if(err){
+            res.send('MySQL connection error.');
+        }
+        res.send('MySQL connection OK.');
+    })
 });
- 
+
 // ----------------------------------------
 app.get("/user/:id", (req, res) => {
     connection.query("SELECT id, username, created_at FROM users WHERE id = ?", [req.params.id] ,(err, results) => {
@@ -42,3 +39,4 @@ app.get("/user/:id", (req, res) => {
         res.json(results);
     })
 });
+

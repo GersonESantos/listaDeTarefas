25c0ed35599ce8a5b77ea84def354c0169eae8d0
GersonESS <gebhsantos@gmail.com>
2025-02-18 13:03:17 -0300

    Update server.js

--- v8/back/server.js
+++ v8/back/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // opcoes de conexao com o MySQL
 const connection = mysql.createConnection({

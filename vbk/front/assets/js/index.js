e46012ec8d5c0e1214282f38c484b7bca033d2ec
GersonESS <gebhsantos@gmail.com>
2025-02-19 11:25:45 -0300

    a

---
+++ vbk/front/assets/js/index.js
@@ -0,0 +1,27 @@
+// javascript do index.html
+let id = 1
+window.onload = () => {
+    
+    get_username(id);
+    get_user_tasks(id);
+}
+
+function get_username(id){
+
+    fetch(`http://localhost:3000/user/tasks/${id}`)
+    .then(response => {
+        if(response.status === 200){
+            return response.json();
+        } else {
+            console.log('ERRO!');
+        }
+    })
+    .then(dados => {
+        if(dados.length === 0){
+            console.log('Erro!');
+        } else {
+            document.querySelector("#username").textContent = dados[0].username;
+        }
+    })
+
+}

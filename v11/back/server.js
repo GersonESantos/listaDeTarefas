89374add88ec8a5070fc3dc696c3ee7b1e4f2d23
GersonESS <gebhsantos@gmail.com>
2025-02-18 22:07:58 -0300

    Update server.js

--- v11/back/server.js
+++ v11/back/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
+
 // opcoes de conexao com o MySQL
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -13,14 +14,14 @@ const app = new express();
 app.listen(3000, () => {
     console.log('Servidor iniciado.');
 })
+
 app.use(cors());
+
 // rotas
 // ----------------------------------------
 app.get("/", (req, res) => {
-
-    // res.send('Olá Mundo!');
     connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
-        if(err){
+        if (err) {
             res.send('MySQL connection error.');
         }
         res.send('MySQL connection OK.');
@@ -29,11 +30,20 @@ app.get("/", (req, res) => {
 
 // ----------------------------------------
 app.get("/user/:id", (req, res) => {
-    connection.query("SELECT id, username, created_at FROM users WHERE id = ?", [req.params.id] ,(err, results) => {
-        if(err){
+    connection.query("SELECT id, username, created_at FROM users WHERE id = ?", [req.params.id], (err, results) => {
+        if (err) {
             res.send('MySQL connection error.');
         }
         res.json(results);
     })
 });
 
+// ----------------------------------------
+app.get("/user/:id/tasks/", (req, res) => {
+    connection.query("SELECT * FROM tasks WHERE id_user = ?", [req.params.id], (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.json(results);
+    })
+});

c05fc61fb30f7133ecd9e75fd1d38a7381db8fcf
GersonESS <gebhsantos@gmail.com>
2025-02-20 23:10:37 -0300

    a

--- gerson/v14/front/assets/js/index.js
+++ gerson/v14/front/assets/js/index.js
@@ -43,37 +43,11 @@ function get_user_tasks(id){
     .then(tarefas => {
         if(tarefas.length === 0){
             document.querySelector("#no_tasks").classList.remove("d-none");
-            document.querySelector("#total_tasks").classList.add("d-none");
         } else {
             
             document.querySelector("#tasks_container").innerHTML = null;
 
-            let colors = [
-                {
-                    task_status: 'new',
-                    select_bg_color: 'bg-white'
-
-                },
-                {
-                    task_status: 'in progress',
-                    select_bg_color: 'bg-info'
-
-                },
-                {
-                    task_status: 'canceled',
-                    select_bg_color: 'bg-danger'
-
-                },
-                {
-                    task_status: 'done',
-                    select_bg_color: 'bg-success'
-
-                },
-            ];
-
             tarefas.forEach(tarefa => {
-
-                let color = colors.find(item => item.task_status == tarefa.task_status);
 
                 let html = `
                 <div class="col-12 border border-secondary rounded p-3 shadow">
@@ -81,19 +55,21 @@ function get_user_tasks(id){
                         <div class="col-8">
                             <div class="d-flex align-items-center">
                                 <h5 class="me-3 text-info"><i class="fa-solid fa-circle-chevron-right"></i></h5>
-                                <h5>${tarefa.task_text}</h5>
+                                <h5>${tarefa.task_description}</h5>
                             </div>
                         </div>
+                    
                         <div class="col-2">
-                            <select id="task_status" class="form-select p-2 ${color.select_bg_color}">
-                                <option value="new" ${tarefa.task_status == 'new' ? 'selected' : ''}>New</option>
-                                <option value="in progress" ${tarefa.task_status == 'in progress' ? 'selected' : ''}>In progress</option>
-                                <option value="canceled" ${tarefa.task_status == 'canceled' ? 'selected' : ''}>Canceled</option>
-                                <option value="done" ${tarefa.task_status == 'done' ? 'selected' : ''}>Done</option>
+                            <select id="task_status" class="form-select p-2">
+                                <option value="new">New</option>
+                                <option value="em progresso" ${tarefa.status == 'em progresso' ? 'selected' : ''}>em progresso</option>
+                                <option value="pendente"     ${tarefa.status == 'pendente' ? 'selected' : ''}>pendente</option>
+                                <option value="concluído"    ${tarefa.status == 'concluído' ? 'selected' : ''}>concluído</option>
                             </select>
                         </div>
-                        <div class="col-1 text-end"><span class="edit_link" data-id-task="${tarefa.id}"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</span></div>
-                        <div class="col-1 text-end"><span class="delete_link" data-id-task="${tarefa.id}"><i class="fa-regular fa-trash-can me-2"></i>Delete</span></div>
+                        <div class="col-1 text-end"><spam class="edit_link"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</spam></div>
+                        
+                        <div class="col-1 text-end text-danger"><i class="fa-regular fa-trash-can me-2"></i>Delete</div>
                     </div>
                 </div>`;
 
@@ -105,8 +81,37 @@ function get_user_tasks(id){
             });
 
             document.querySelector("#no_tasks").classList.add("d-none");
-            document.querySelector("#total_tasks").classList.remove("d-none");
-            document.querySelector("#total_tasks > div > h4 > span").textContent = tarefas.length;
+            console.log(tarefas.length);
+            document.querySelector("#total_tasks").textContent = tarefas.length;
+            //console.log(document.querySelector("#total_tasks"))
+            
         }
     })
+
 }
+
+
+/* 
+            <div class="row mb-3">
+                <div class="col-12 border border-secondary rounded p-3 shadow">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <div class="d-flex align-items-center">
+                                <h5 class="me-3 text-info"><i class="fa-solid fa-circle-chevron-right"></i></h5>
+                                <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                            <select id="task_status" class="form-select p-2">
+                                <option value="new">New</option>
+                                <option value="in progress">In progress</option>
+                                <option value="canceled">Canceled</option>
+                                <option value="done">Done</option>
+                            </select>
+                        </div>
+                        <div class="col-1 text-end"><i class="fa-regular fa-pen-to-square me-2"></i>Edit</div>
+                        <div class="col-1 text-end text-danger"><i class="fa-regular fa-trash-can me-2"></i>Delete</div>
+                    </div>
+                </div>
+            </div>
+*/

2350d9663c350803690d56c7dc0e940ef4431b92
GersonESS <gebhsantos@gmail.com>
2025-02-23 01:49:54 -0300

    a

---
+++ roxo/r9 1/back/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+// opcoes de conexao com o MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'bd_tasks'
+});
+
+const app = new express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.send('MySQL conectou OK. digite http://localhost:3000/user/1');
+    })
+});
+
+// ----------------------------------------
+app.get("/user/:id", (req, res) => {
+    connection.query("SELECT id, username, created_at FROM users WHERE id = ?", [req.params.id], (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.json(results);
+    })
+});
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Rodando app listening at http://localhost:3000');
+  });

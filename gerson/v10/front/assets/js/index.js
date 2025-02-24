356e474fa8a1503734cd06d818b2de728778fae6
GersonESS <gebhsantos@gmail.com>
2025-02-24 15:16:56 -0300

    a

--- gerson/v10/front/assets/js/index.js
+++ gerson/v10/front/assets/js/index.js
@@ -8,6 +8,26 @@ window.onload = () => {
 
 function get_username(id){
 
+    fetch(`http://localhost:3000/user/${id}`)
+    .then(response => {
+        if(response.status === 200){
+            return response.json();
+        } else {
+            console.log('ERRO!');
+        }
+    })
+    .then(dados => {
+        if(dados.length === 0){
+            console.log('Erro!');
+        } else {
+            document.querySelector("#username").textContent = dados[0].username;
+        }
+    })
+
+}
+
+function get_user_tasks(id){
+
     fetch(`http://localhost:3000/user/tasks/${id}`)
     .then(response => {
         if(response.status === 200){

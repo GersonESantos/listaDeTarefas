1c96a23f19dd2e598939d44b44fd0f39882448c3
GersonESS <gebhsantos@gmail.com>
2025-02-25 01:07:22 -0300

    a

---
+++ roxo/r76/back/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+// opcoes de conexao com o MySQL
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Gabibi89*',
+    database: 'bd_tasks'
+});
+
+const app = new express();
+
+
+app.use(cors());
+
+// rotas
+// ----------------------------------------
+app.get("/", (req, res) => {
+    connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.send('MySQL connection OK.');
+    })
+});
+
+// ----------------------------------------
+app.get("/user/:id", (req, res) => {
+    connection.query("SELECT * FROM users WHERE id = ?", [req.params.id], (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.json(results);
+    })
+});
+
+// ----------------------------------------
+app.get("/user/:id/tasks/", (req, res) => {
+    connection.query("SELECT * FROM tasks WHERE id_user = ?", [req.params.id], (err, results) => {
+        if (err) {
+            res.send('MySQL connection error.');
+        }
+        res.json(results);
+    })
+});
+// Nova rota para login por email
+app.get("/login", (req, res) => {
+    const email = req.query.email;
+    connection.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+        if (err) {
+            res.status(500).send('MySQL connection error.');
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('🚀Rodando server/r75 listening at http://localhost:3000');
+  });

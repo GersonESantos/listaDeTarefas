2b4db3bd947c6f0b986b2ac6885c6292b615a92f
GersonESS <gebhsantos@gmail.com>
2025-02-27 15:31:17 -0300

    Update server.js

--- roxo/r78/back/server.js
+++ roxo/r78/back/server.js
@@ -58,5 +58,5 @@ app.get("/login", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('🚀Rodando server/r76 listening at http://localhost:3000');
+    console.log('🚀Rodando server/r78 listening at http://localhost:3000');
   });

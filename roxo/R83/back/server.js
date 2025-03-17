0b8a6553b3ca26dc8c6a0b2dc3571a7a1facfff5
GersonESS <gebhsantos@gmail.com>
2025-03-17 11:58:48 -0300

    Update server.js

--- roxo/R83/back/server.js
+++ roxo/R83/back/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 
-// Opções de conexão com o MySQL
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,13 +9,11 @@ const connection = mysql.createConnection({
     database: 'bd_tasks'
 });
 
-const app = express(); // Corrigido: "new express()" para "express()"
+const app = express();
 
 app.use(cors());
-app.use(express.json()); // Adicionado para parsing de JSON nas requisições DELETE
+app.use(express.json());
 
-// Rotas
-// ----------------------------------------
 app.get("/", (req, res) => {
     connection.query("SELECT COUNT(*) users FROM users", (err, results) => {
         if (err) {
@@ -26,7 +23,6 @@ app.get("/", (req, res) => {
     });
 });
 
-// ----------------------------------------
 app.get("/user/:id", (req, res) => {
     connection.query("SELECT * FROM users WHERE id = ?", [req.params.id], (err, results) => {
         if (err) {
@@ -36,7 +32,6 @@ app.get("/user/:id", (req, res) => {
     });
 });
 
-// ----------------------------------------
 app.get("/user/:id/tasks/", (req, res) => {
     connection.query("SELECT * FROM tasks WHERE id_user = ?", [req.params.id], (err, results) => {
         if (err) {
@@ -46,21 +41,24 @@ app.get("/user/:id/tasks/", (req, res) => {
     });
 });
 
-// ----------------------------------------
-// Rota para login por email
+// Rota de login ajustada para usar 'passwd'
 app.get("/login", (req, res) => {
-    const email = req.query.email;
-    connection.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
-        if (err) {
-            res.status(500).send('MySQL connection error.');
-        } else {
-            res.json(results);
+    const { email, password } = req.query; // 'password' vem do front-end
+    connection.query(
+        "SELECT * FROM users WHERE email = ? AND passwrd = ?", // Mudado de 'password' para 'passwd'
+        [email, password],
+        (err, results) => {
+            if (err) {
+                res.status(500).send('MySQL connection error.');
+            } else if (results.length > 0) {
+                res.json(results);
+            } else {
+                res.status(401).send('Email ou senha inválidos.');
+            }
         }
-    });
+    );
 });
 
-// ----------------------------------------
-// Nova rota para excluir uma tarefa
 app.delete("/tasks/:id", (req, res) => {
     const taskId = req.params.id;
     connection.query("DELETE FROM tasks WHERE id = ?", [taskId], (err, result) => {
@@ -77,5 +75,5 @@ app.delete("/tasks/:id", (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('🚀 Rodando server/r81 listening at http://localhost:3000');
+    console.log('🚀 Rodando server/r83 listening at http://localhost:3000');
 });

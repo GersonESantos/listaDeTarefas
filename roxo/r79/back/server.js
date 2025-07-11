f4af73786397da592891100cde558669d3db181c
GersonESS <gebhsantos@gmail.com>
2025-07-11 19:31:22 -0300

    Update server.js

--- roxo/r79/back/server.js
+++ roxo/r79/back/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
 
 // opcoes de conexao com o MySQL
 const connection = mysql.createConnection({
